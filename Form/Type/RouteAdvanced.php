b4700089b995b6ef568915aad625984f850ed0f5
ElectricMaxxx <maximilian.berghoff@gmx.de>
2016-06-20 07:34:06 +0200

    create form types for route advanced section and redirect route admin

---
+++ Form/Type/RouteAdvanced.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2015 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Cmf\Bundle\RoutingBundle\Form\Type;
+
+use Sonata\CoreBundle\Form\Type\ImmutableArrayType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Maximilian Berghoff <Maximilian.Berghoff@mayflower.de>
+ */
+class RouteAdvanced extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $keys = array_key_exists('keys', $options) ? $options['keys'] : [];
+        $builder
+            ->add('variablePattern', TextType::class, ['required' => false], ['help' => 'form.help_variable_pattern'])
+            ->add('defaults', ImmutableArrayType::class, ['keys' => $keys])
+            ->add('options', ImmutableArrayType::class, ['keys' => $keys], ['help' => 'form.help_options'])
+        ;
+    }
+
+    /**
+     * Configures the options for this type.
+     *
+     * @param OptionsResolver $resolver The resolver for the options.
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'keys' => []
+        ]);
+    }
+}
